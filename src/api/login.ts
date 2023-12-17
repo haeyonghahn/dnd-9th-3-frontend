e9c97fa8d4b17ff7c5dafa30ca05da73c4c2aac6
haeyonghahn <yong80211@gmail.com>
2023-12-17 19:08:54 +0900

    feat: 로그인 페이지 추가

---
+++ src/api/login.ts
@@ -0,0 +1,47 @@
+import { User } from "../types/user";
+import { BASE_URL } from "./const";
+
+type LoginResult = "success" | "fail";
+
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+/* options 예시:
+const option: RequestInit = {
+  method: 'POST',
+  body: JSON.stringify({ username, password })
+} */
+
+const fetchClient = async (url: string, options: RequestInit) => {
+  return fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      credentials: "include",
+    },
+    ...options,
+  });
+};
+
+export const login = async (args: LoginRequest): Promise<LoginResult> => {
+  const loginRes = await fetchClient(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    body: JSON.stringify(args),
+  });
+
+  return loginRes.ok ? "success" : "fail";
+};
+
+export const getCurrentUserInfo = async (): Promise<User | null> => {
+  try {
+    const userInfoRes = await fetchClient(`${BASE_URL}/profile`, {
+      method: "GET",
+    });
+
+    return userInfoRes.ok ? userInfoRes.json() : null;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
---
+++ src/layout/GeneralLayoutProps.tsx
@@ -0,0 +1,42 @@
+import { getCurrentUserInfo } from "@/api/login";
+import Sidebar from "@/components/Sidebar";
+import { useRouter } from "@/hooks/useRouter";
+import { SidebarContent } from "@/router";
+import { User } from "@/types/user";
+import { useCallback, useEffect, useState } from "react";
+
+interface GeneralLayoutProps {
+  children: React.ReactNode;
+}
+
+const GeneralLayout: React.FC<GeneralLayoutProps> = ({ children }) => {
+  const [userProfile, setUserProfile] = useState<User | null>(null);
+  const { routeTo } = useRouter();
+
+  const fetchUserProfile = useCallback(async () => {
+    const userProfileResponse = await getCurrentUserInfo();
+
+    if (userProfileResponse === null) {
+      routeTo("/login");
+      return;
+    }
+
+    setUserProfile(userProfileResponse);
+  }, []);
+
+  useEffect(() => {
+    console.log("page changed!");
+    fetchUserProfile();
+  }, [children]);
+
+  if (!userProfile) return <div>loading...</div>;
+
+  return (
+    <div className="general-layout">
+      <Sidebar sidebarContent={SidebarContent} userProfile={userProfile} />
+      <div className="general-layout__body">{children}</div>
+    </div>
+  );
+};
+
+export default GeneralLayout;
