3c29df14290df2ed2392386e4688d4dfb8c5834d
haeyonghahn <yong80211@gmail.com>
2024-05-20 21:28:33 +0900

    Feat: Mock API 추가

--- src/api/const.ts
+++ src/api/const.ts
@@ -1,4 +1,3 @@
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
 export const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-export const NODE_ENV = import.meta.env.VITE_NODE_ENV;
