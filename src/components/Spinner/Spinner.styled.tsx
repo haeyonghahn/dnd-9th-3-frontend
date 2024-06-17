8bbbc87cc5164a943247c7d30508f2d316bac112
haeyonghahn <yong80211@gmail.com>
2024-06-18 00:31:41 +0900

    Fix: Spinner 화면 정중앙 배치

--- src/components/Spinner/Spinner.styled.tsx
+++ src/components/Spinner/Spinner.styled.tsx
@@ -1,7 +1,12 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Svg = styled(motion.svg)`
   display: block;
